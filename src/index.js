b17017895601baa9292725d97e673fd9c0ac1f88
dineshkota3 <dineshkota3@gmail.com>
2020-07-01 21:11:13 +0530

    Add the research for cookie

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Dinesh from './Dinesh.js'
+import Model from './Car.js'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ========================================
+const mycar = new Model("Ford", "Mustang");
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Dinesh />, document.getElementById('root'));
+ReactDOM.render(mycar.show(), document.getElementById('mydiv'))
+
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+if (getCookie("dinesh")==="") {
+  var cookievalue = Math.random()
+  console.log(cookievalue)
+  document.cookie = "dinesh=" + cookievalue;
+  document.write ("Setting Cookies : " + "dinesh=" + cookievalue );
+}
+
+document.write ("get Cookies : " + "dinesh=" + getCookie("dinesh") );
